72877b6339f770b9680983ce856d7871346f797f
saurabhAMD <saurabh.roychowdhury@amd.com>
2024-07-30 21:45:31 -0500

    implementing all reduce for all ranks less than equal to 8

--- tools/mtype_test/allreduce_mtype.cpp
+++ tools/mtype_test/allreduce_mtype.cpp
@@ -48,10 +48,325 @@ struct kernelParam
     void *ptrs_done[7];
 };
 
+__global__ void allred0Peer(float* in, float *out, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred1Peer(float* in, float *out, float* peer1, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred2Peer(float* in, float *out, float* peer1, float* peer2, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+        sum += peer2[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred3Peer(float* in, float *out, float* peer1, float* peer2, float* peer3, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+        sum += peer2[i];
+        sum += peer3[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred4Peer(float* in, float *out, float* peer1, float* peer2, float* peer3, float* peer4, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+        sum += peer2[i];
+        sum += peer3[i];
+        sum += peer4[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred5Peer(float* in, float *out, float* peer1, float* peer2, float* peer3, float* peer4, float* peer5, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+        sum += peer2[i];
+        sum += peer3[i];
+        sum += peer4[i];
+        sum += peer5[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
+__global__ void allred6Peer(float* in, float *out, float* peer1, float* peer2, float* peer3, float* peer4, float* peer5, float* peer6, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
+{
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+	int tid = hipThreadIdx_x;
+
+	/* sync before starting the reduction */
+	if (tid < nranks) {
+		if (hipBlockIdx_x == 0 && tid == 0) {
+			flag[myrank] = iter;
+#pragma unroll
+			for (int j=0; j < (nranks-1); j++) {
+				__atomic_store_n(&(((int*) kp.ptrs_flag[j])[myrank]), iter, __ATOMIC_RELEASE);
+			}	
+		}
+		/* atomic load serializes the reads among the threads. Also, flag is uncached. */
+		while(__atomic_load_n(&flag[tid], __ATOMIC_ACQUIRE) != iter) {}
+	}
+	__syncthreads();
+
+
+	float sum;
+	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
+		sum = in[i];
+		sum += peer1[i];
+        sum += peer2[i];
+        sum += peer3[i];
+        sum += peer4[i];
+        sum += peer5[i];
+        sum += peer6[i];
+		out[i] = sum;
+	}
+
+	/* sync after finishing the reduction */
+	if (tid < nranks) {
+                if (hipBlockIdx_x == 0 && tid == 0) {
+                        done[myrank] = iter;
+#pragma unroll
+                        for (int j=0; j < (nranks-1); j++) {
+                __atomic_store_n(&(((int*) kp.ptrs_done[j])[myrank]), iter, __ATOMIC_RELEASE);
+                        }
+                }
+
+                while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
+        }
+        __syncthreads();
+
+}
+
 __global__ void allred7Peer(float* in, float *out, float* peer1, float* peer2, float* peer3, float* peer4, float* peer5, float* peer6, float* peer7, 
 		int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
 {
-        int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
+    int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
 
 	int tid = hipThreadIdx_x;
 
@@ -99,12 +414,11 @@ __global__ void allred7Peer(float* in, float *out, float* peer1, float* peer2, f
 
                 while(__atomic_load_n(&done[tid], __ATOMIC_ACQUIRE) != iter) {}
         }
-        __syncthreads();
+    __syncthreads();
 
 }
 
-__global__ void allred2Peer(float* in, float *out, float* peer1, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter)
-{
+__global__ void allredPeer(float* in, float *out, int* done, int *flag, kernelParam kp, int myrank, int nranks, int iter, int numDevices) {
     int num = hipThreadIdx_x + hipBlockDim_x * hipBlockIdx_x;
 	int tid = hipThreadIdx_x;
 
@@ -126,7 +440,9 @@ __global__ void allred2Peer(float* in, float *out, float* peer1, int* done, int 
 	float sum;
 	for (int i = num; i < N; i+= hipBlockDim_x * hipGridDim_x) {
 		sum = in[i];
-		sum += peer1[i];
+        for (int j=0;j< numDevices; j++) {
+            sum += ((float *)kp.ptrs[j])[i];
+        }
 		out[i] = sum;
 	}
 
@@ -158,18 +474,24 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     kernelParam kp;
 
-
-
     hipDeviceProp_t devProp;
 
     HIP_CALL(hipSetDevice(myrank));
     hipGetDeviceProperties(&devProp, myrank);
 
+    int numDevices = 0;
+    hipError_t result = hipGetDeviceCount(&numDevices);
+    if (result == hipSuccess) {
+        std::cout << "Number of devices: " << numDevices << ", nranks= "<< nranks << std::endl;
+    } else {
+        std::cerr << "Error: " << result << std::endl;
+    }
+
     float* input;
     input = (float*) malloc(N*sizeof(float));
 
     for (int i = 0; i < N; i++) {
-	input[i] = myrank + 1;
+	    input[i] = myrank + 1;
     }
     float *output = (float*) malloc(N*sizeof(float));
     for (int i = 0; i < N; i++) {
@@ -179,7 +501,7 @@ int main(int argc, char* argv[])
     int *flag;
     flag= (int*) malloc (nranks * sizeof(int));
     for (int i = 0; i < nranks; i++) {
-	flag[i] = 0;
+	    flag[i] = 0;
     }
 
     float* inputBuffer;
@@ -225,21 +547,17 @@ int main(int argc, char* argv[])
 
 
     for (int i = 0; i < nranks; i++) {
-	if (i != myrank) {
+        if (i != myrank) {
     		HIP_CALL(hipIpcOpenMemHandle(&(kp.ptrs[j]), ipc_handles[i], hipIpcMemLazyEnablePeerAccess));
     		HIP_CALL(hipIpcOpenMemHandle(&(kp.ptrs_done[j]), done_handles[i], hipIpcMemLazyEnablePeerAccess));				
     		HIP_CALL(hipIpcOpenMemHandle(&(kp.ptrs_flag[j++]), flag_handles[i], hipIpcMemLazyEnablePeerAccess));				
-	}
+	    }
     }
-
-
 
     hipStream_t stream;
     hipStreamCreate(&stream);
-
     MPI_Barrier(MPI_COMM_WORLD);
 
-    
     // /* Warmup */
     // for (int iter = 0; iter < 10; iter++) {
 	//     hipLaunchKernelGGL(allred7Peer,
@@ -251,30 +569,86 @@ int main(int argc, char* argv[])
     //             flagBuffer, kp, myrank, nranks, iter+1);
     // }
     // hipStreamSynchronize(stream);
-
-
     // MPI_Barrier(MPI_COMM_WORLD);
-
-    
     
     /* start time measurement */
     t1 = std::chrono::high_resolution_clock::now();
      
     /* Allreduce using read */
     for (int iter = 0; iter < 1; iter++) {	
-    // hipLaunchKernelGGL(allred7Peer,
-    //     dim3(min(32,(N)/256)),
-    //     dim3(256),
-    //     0, stream, 
-    //     inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]),
-    //         (float*)(kp.ptrs[3]), (float*)(kp.ptrs[4]), (float*)(kp.ptrs[5]), (float*)(kp.ptrs[6]), doneBuffer,
-    //         flagBuffer, kp, myrank, nranks, iter+1);
-    hipLaunchKernelGGL(allred2Peer,
-        dim3(min(32,(N)/256)),
-        dim3(256),
-        0, stream, 
-        inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), doneBuffer,
-            flagBuffer, kp, myrank, nranks, iter+1);
+        if(nranks==1) {
+            hipLaunchKernelGGL(allred0Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==2) {
+            hipLaunchKernelGGL(allred1Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==3) {
+            hipLaunchKernelGGL(allred2Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==4) {
+            hipLaunchKernelGGL(allred3Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==5) {
+            hipLaunchKernelGGL(allred4Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]), (float*)(kp.ptrs[3]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==6) {
+            hipLaunchKernelGGL(allred5Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]), (float*)(kp.ptrs[3]), (float*)(kp.ptrs[4]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==7) {
+            hipLaunchKernelGGL(allred6Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]), (float*)(kp.ptrs[3]), (float*)(kp.ptrs[4]), (float*)(kp.ptrs[5]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        }
+        else if(nranks==8) {
+            hipLaunchKernelGGL(allred7Peer,
+                dim3(min(32,(N)/256)),
+                dim3(256),
+                0, stream, 
+                inputBuffer, outputBuffer, (float*)(kp.ptrs[0]), (float*)(kp.ptrs[1]), (float*)(kp.ptrs[2]), (float*)(kp.ptrs[3]), (float*)(kp.ptrs[4]), (float*)(kp.ptrs[5]), (float*)(kp.ptrs[6]), doneBuffer,
+                    flagBuffer, kp, myrank, nranks, iter+1);
+        } 
+        // else {
+        //     cout<<"nranks > 8 used. no allreduce being run."<<endl;
+        // }
+        // hipLaunchKernelGGL(allredPeer,
+        //     dim3(min(32,(N)/256)),
+        //     dim3(256),
+        //     0, stream, 
+        //     inputBuffer, outputBuffer, doneBuffer,
+        //         flagBuffer, kp, myrank, nranks, iter+1, numDevices);
     }
     hipStreamSynchronize(stream);
 
@@ -287,17 +661,18 @@ int main(int argc, char* argv[])
     MPI_Allreduce(&times, &out, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     
     if (myrank == 0) 
-    	//cout<< "Allred time for 8 ranks = " << out/nranks << " data size = " << N*sizeof(float) << endl; //checks performance
-        cout<< "Allred time for 2 ranks = " << out/nranks << " data size = " << N*sizeof(float) << endl; //checks performance
+        cout<< "Allreduce time for "<< nranks << " ranks = " << out/nranks << " data size = " << N*sizeof(float) << endl; //checks performance
 
 
     hipMemcpy(output, outputBuffer, N * sizeof(float), hipMemcpyDeviceToHost);
 
     float *p = (float*) output;
+    bool testRest = true;
     for (int i = 0; i < N; i++) {
     	if (p[i] != nranks*(nranks+1)/2) {
         	cout<< "Wrong result " << p[i] << " at index "<< i << endl; //checks correctness
-                break;
+            testRest = false;
+            break;
         }
     }
 
@@ -306,7 +681,8 @@ int main(int argc, char* argv[])
     hipFree(outputBuffer);
     free(output);
 
-    std::cout<<"Passed!\n";
+    if(testRest) std::cout<<"Passed!\n";
+    else std::cout<<"Failed!\n";
 
 
     MPI_Finalize();
